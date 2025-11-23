5de2fedb6deb31635919e8957eb7a8db90513635
Riyan-gif <riyanpolara72@gmail.com>
2025-11-23 16:13:24 +0530

    Initail Comit

---
+++ Assignment-3-Task-2.py
@@ -0,0 +1,9 @@
+import math
+
+number= int(input("Enter a number: "))
+sq= math.sqrt(number)
+log= math.log(number)
+sin= math.sin(number)
+print(f"Square root : {sq}")
+print(f"logarithm : {log}")
+print(f"Sine : {sin}")
